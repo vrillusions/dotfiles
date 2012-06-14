6d22f1c1487240dd4b06d1244d4f9e8045c6424f
Todd Eddy <vr@vrillusions.com>
2012-06-14 14:01:15 -0400

    (vim) python template now encourages use of logging module.

--- vim/templates/tmpl.py
+++ vim/templates/tmpl.py
@@ -9,6 +9,7 @@ This is a template for python.
 
 import sys
 import traceback
+import logging
 
 
 __version__ = 'alpha'
@@ -16,22 +17,29 @@ __version__ = 'alpha'
 
 def main():
     """The main function."""
+    log = logging.getLogger('main()')
+    log.debug('Printing hello world to screen')
     print "hello world!"
 
 
 if __name__ == "__main__":
+    # DEBUG, WARNING, ERROR, or CRITICAL
+    logging.basicConfig(level=logging.ERROR)
+    log = logging.getLogger('ifmain')
     try:
         main()
     except KeyboardInterrupt, e:
         # Ctrl-c
+        log.error('Received keyboard interupt')
         raise e
     except SystemExit, e:
         # sys.exit()
+        log.debug('Received sys.exit()')
         raise e
     except Exception, e:
-        print "ERROR, UNEXPECTED EXCEPTION"
-        print str(e)
-        traceback.print_exc()
+        log.error("ERROR, UNEXPECTED EXCEPTION")
+        log.error(str(e))
+        log.error(traceback.format_exc())
         sys.exit(1)
     else:
         # Main function is done, exit cleanly
