16026fe331c85f0e73b0a95e6f26f8650d175425
Todd Eddy <vr@vrillusions.com>
2013-10-25 00:47:20 -0400

    vim: Change createlinks.bat to create symlinks
    To try and get around a bug in windows it made hard links. After
    updating to v7.4 it appears to work using normal symlinks.

--- createlinks.bat
+++ createlinks.bat
@@ -1,9 +1,12 @@
 @ECHO OFF
 REM This needs to run as administrator
-REM make a couple symlinks to dotfiles
+REM make a couple symlinks to dotfiles, mainly just vim configs.
 cd %HOMEPATH%
-mklink /H _vimrc "dotfiles\vimrc"
-mklink /H _gvimrc "dotfiles\gvimrc"
+REM The symlink issue appears fixed in v7.4 so just creating normal symlinks.
+REM Before this uses the /H option to create hard links but then it's not
+REM obvious you're changing a hard-linked file. Plus it didn't work either.
+mklink _vimrc "dotfiles\vimrc"
+mklink _gvimrc "dotfiles\gvimrc"
 REM directory doesn't need a hard link
 mklink /D .vim "dotfiles\vim"
 pause
