22b2f976dc47762dfbac4f28ef01f9914ffa5929
Todd Eddy <vr@vrillusions.com>
2013-11-30 01:30:41 -0500

    Change git symlinks to use new xdg config directory
    Now git configs are stored in ~/.config/git. Hopefully more programs
    will use this which will cleanup home directory. Also added a gitingore
    file for common OS files to ignore.

--- createlinks.sh
+++ createlinks.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # src: https://github.com/jpalardy/dotfiles/blob/master/.symlink_it.sh
 
+# upon attempt to use an unset variable, print error and exit
+set -u
+
 # note to self: removing since I don't know the purpose of this
 #flag="$@"
 
@@ -9,7 +12,22 @@ function relink() {
   ln -s -n -v $2 $1
 }
 
-DOTFILES=$(pwd)
+# Checks if directory exists, if not creates it with minimal permissions.
+#
+# Note: This intentionally doesn't create parent directories. Those should be
+# checked individually so permissions are only changed if we are the ones
+# making the directory
+#
+# Usage: create_dir "/full/path/to/directory"
+function create_dir() {
+    if [ ! -d "$1" ]; then
+        mkdir -m 0700 "$1"
+    fi
+}
+
+# Get the directory this script is in
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOTFILES=${SCRIPT_DIR}
 
 # First create a gitconfig if doesn't exist
 if [ ! -f gitconfig ]; then
@@ -17,7 +35,7 @@ if [ ! -f gitconfig ]; then
     cp gitconfig.versioned gitconfig
 fi
 
-cd
+cd ${HOME}
 
 relink .bash                   $DOTFILES/bash
 relink .bashrc                 $DOTFILES/bashrc
@@ -32,11 +50,20 @@ relink .lftprc                 $DOTFILES/lftprc
 relink .gemrc                  $DOTFILES/gemrc
 relink .rvmrc                  $DOTFILES/rvmrc
 
-relink .gitconfig              $DOTFILES/gitconfig
-#relink .gitignore              $DOTFILES/gitignore
+# Use XDG directories for git. Requires v1.7.12+
+if [ -f "${HOME}/.gitconfig" ]; then
+    echo "~/.gitconfig already exists, not using new config structure"
+else
+    create_dir "${HOME}/.config"
+    create_dir "${HOME}/.config/git"
+    cd .config/git
+    relink config                  $DOTFILES/gitconfig
+    relink ignore                  $DOTFILES/gitignore
+    cd ${HOME}
+fi
 
 echo
-echo "Note: you need to manually link procmail and spamassassin files on mail server."
-echo "Also gvimrc if you need that."
+echo "This only sets up common files. Additional files to setup if you use them"
+echo "include procmail, spamassassin, and gvimrc"
 
 exit 0
