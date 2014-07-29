ef4a4dd0d7b819ba029f8f1912db8c8871967a04
Todd Eddy <vr@vrillusions.com>
2014-07-29 08:59:15 -0400

    Refactor createlinks.sh
    - updated to follow current styleguide
    - create xdg basedir directories

--- createlinks.sh
+++ createlinks.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 # src: https://github.com/jpalardy/dotfiles/blob/master/.symlink_it.sh
 
-# upon attempt to use an unset variable, print error and exit
+set -e
 set -u
 
-# note to self: removing since I don't know the purpose of this
-#flag="$@"
 
+# Get the directory this script is in
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+dotfiles="${script_dir}"
+
+
+# Usage: relink "destination" "source"
 function relink() {
-  # ln -s -h -v $flag $2 $1
-  ln -s -n -v $2 $1
+  ln -s -n -v $2 $1 || true
 }
 
 # Checks if directory exists, if not creates it with minimal permissions.
 #
-# Note: This intentionally doesn't create parent directories. Those should be
-# checked individually so permissions are only changed if we are the ones
-# making the directory
+# Note: This intentionally doesn't create parent directories. Reason is with the
+# -p option it will create the parent directories but with default permissions.
+# It will only use the mode specified with -m for the final folder. A possible
+# future enhancement is to make this a recursive function.
 #
 # Usage: create_dir "/full/path/to/directory"
 function create_dir() {
@@ -25,41 +29,43 @@ function create_dir() {
     fi
 }
 
-# Get the directory this script is in
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DOTFILES=${SCRIPT_DIR}
 
-# First create a gitconfig if doesn't exist
+cd "${script_dir}"
 if [ ! -f gitconfig ]; then
     echo "creating local .gitconfig"
     cp gitconfig.versioned gitconfig
 fi
 
-cd ${HOME}
+cd "${HOME}"
+relink .bash         "${dotfiles}/bash"
+relink .bashrc       "${dotfiles}/bashrc"
+relink .bash_profile "${dotfiles}/bash_profile"
+relink .inputrc      "${dotfiles}/inputrc"
 
-relink .bash                   $DOTFILES/bash
-relink .bashrc                 $DOTFILES/bashrc
-relink .bash_profile           $DOTFILES/bash_profile
-relink .inputrc                $DOTFILES/inputrc
+relink .vim          "${dotfiles}/vim"
+relink .vimrc        "${dotfiles}/vimrc"
 
-relink .vim                    $DOTFILES/vim
-relink .vimrc                  $DOTFILES/vimrc
+relink .screenrc     "${dotfiles}/screenrc"
+relink .lftprc       "${dotfiles}/lftprc"
+relink .gemrc        "${dotfiles}/gemrc"
+relink .rvmrc        "${dotfiles}/rvmrc"
 
-relink .screenrc               $DOTFILES/screenrc
-relink .lftprc                 $DOTFILES/lftprc
-relink .gemrc                  $DOTFILES/gemrc
-relink .rvmrc                  $DOTFILES/rvmrc
+
+# Create XDG Basedir directories if they don't exist
+create_dir "${HOME}/.cache"
+create_dir "${HOME}/.config"
+create_dir "${HOME}/.local"
+create_dir "${HOME}/.local/share"
 
 # Use XDG directories for git. Requires v1.7.12+
 if [ -f "${HOME}/.gitconfig" ]; then
     echo "~/.gitconfig already exists, not using new config structure"
 else
-    create_dir "${HOME}/.config"
     create_dir "${HOME}/.config/git"
-    cd .config/git
-    relink config                  $DOTFILES/gitconfig
-    relink ignore                  $DOTFILES/gitignore
-    cd ${HOME}
+    cd "${HOME}/.config/git"
+    relink config "${dotfiles}/gitconfig"
+    relink ignore "${dotfiles}/gitignore"
+    cd "${HOME}"
 fi
 
 echo
