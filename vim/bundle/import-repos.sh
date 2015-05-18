169913b09149e77f1775434c8db093d6dcfb4678
Todd Eddy <vr@vrillusions.com>
2015-05-17 23:35:40 -0400

    vim: Add localrc plugin
    Allows placing .local.vimrc files anywhere in directory tree to override
    settings

--- vim/bundle/import-repos.sh
+++ vim/bundle/import-repos.sh
@@ -132,6 +132,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     import_repo "https://github.com/moll/vim-bbye.git" bbye v1.0.1
     import_repo "https://github.com/ngn/vim-buffing-wheel.git" buffing-wheel 130cf196
     import_repo "https://github.com/kien/ctrlp.vim.git" ctrlp 1.79
+    import_repo "https://github.com/thinca/vim-localrc.git" localrc v0.2.0
     import_repo "https://github.com/scrooloose/nerdtree.git" nerdtree 4.2.0
     import_repo "https://github.com/ciaranm/securemodelines.git" securemodelines bfbecf0
     import_repo "https://github.com/tmhedberg/SimpylFold.git" SimpylFold 08d94d4
