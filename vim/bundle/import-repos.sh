3c792fd58c972e764eeb4a08512e45cd800c5b00
Todd Eddy <vr@vrillusions.com>
2015-10-27 15:37:08 -0400

    vim: Bring securemodelines up to date

--- vim/bundle/import-repos.sh
+++ vim/bundle/import-repos.sh
@@ -134,7 +134,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     import_repo "https://github.com/kien/ctrlp.vim.git" ctrlp 1.79
     import_repo "https://github.com/thinca/vim-localrc.git" localrc v0.2.0
     import_repo "https://github.com/scrooloose/nerdtree.git" nerdtree 4.2.0
-    import_repo "https://github.com/ciaranm/securemodelines.git" securemodelines bfbecf0
+    import_repo "https://github.com/ciaranm/securemodelines.git" securemodelines 9751f29
     import_repo "https://github.com/tmhedberg/SimpylFold.git" SimpylFold 08d94d4
     import_repo "https://github.com/altercation/vim-colors-solarized.git" solarized 528a59f
     import_repo "https://github.com/ervandew/supertab.git" supertab b0ca47f
