58018cfce5523d6fb1e16924b5251e349ead711c
Todd Eddy <vr@vrillusions.com>
2018-11-06 18:14:09 -0500

    Update cask list and os x terminal profile
    * Recent versions make it easier to get a list of casks so no need for
      a temp file
    
    * Enable bright colors for bold text in terminal

--- script/osx/osx_homebrew_cask_list.sh
+++ script/osx/osx_homebrew_cask_list.sh
@@ -14,16 +14,8 @@ if ! command -v brew cask >/dev/null; then
 fi
 
 
-# -- tmpfile used since piping `cask info` doesn't work --
-readonly script_name="$(basename $0)"
-readonly tmpfile="$(mktemp ${script_name}.XXXXXXXXXX)"
-trap "rm -f ${tmpfile}" EXIT
-echo $tmpfile
-
-
 for cask in $(brew cask list); do
-    brew cask info "${cask}" >"${tmpfile}"
-    head -1 "${tmpfile}"
+    head -1 <(brew cask info "${cask}")
 done
 
 exit 0
